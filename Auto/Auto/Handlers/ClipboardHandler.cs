c34a7a020e123446d57e09f51ffb44a0cbe57b5d
hvards <loud.gift9305@fastmail.com>
2024-03-24 19:31:50 +0100

    Release all keys to ensure that highlighted text is copied

--- Auto/Auto/Handlers/ClipboardHandler.cs
+++ Auto/Auto/Handlers/ClipboardHandler.cs
@@ -1,43 +1,47 @@
 ﻿using Clipboard = Windows.ApplicationModel.DataTransfer.Clipboard;
 
 namespace Auto.Handlers;
+
 public static class ClipboardHandler
 {
-    private const int COPY_DELAY = 75;
+	private const int COPY_DELAY = 75;
 
-    public static string GetClipboardText(bool copyHighlightedText = false)
-    {
-        if (copyHighlightedText)
-            KeyboardHandler.CopyHighlightedText();
+	public static string GetClipboardText(bool copyHighlightedText = false)
+	{
+		if (copyHighlightedText)
+		{
+			KeyboardHandler.ReleaseAllKeys();
+			KeyboardHandler.CopyHighlightedText();
+		}
 
-        var clipboardText = StaHandler.Execute(RetrieveClipboardText);
+		var clipboardText = StaHandler.Execute(RetrieveClipboardText);
 
-        if (copyHighlightedText)
-            DeleteClipboard();
+		if (copyHighlightedText)
+			DeleteClipboard();
 
-        return clipboardText?.Trim();
-    }
+		return clipboardText?.Trim();
+	}
 
-    private static void DeleteClipboard() => StaHandler.Execute(ResetClipboard);
+	private static void DeleteClipboard() => StaHandler.Execute(ResetClipboard);
 
-    private static string RetrieveClipboardText()
-    {
-        Thread.Sleep(COPY_DELAY);
-        return System.Windows.Forms.Clipboard.GetText();
-    }
+	private static string RetrieveClipboardText()
+	{
+		Thread.Sleep(COPY_DELAY);
+		return System.Windows.Forms.Clipboard.GetText();
+	}
 
-    private static async Task<bool> ResetClipboard()
-    {
-        Thread.Sleep(500 + COPY_DELAY);
+	private static async Task<bool> ResetClipboard()
+	{
+		Thread.Sleep(500 + COPY_DELAY);
 
-        var history = await Clipboard.GetHistoryItemsAsync();
-        var t = history.Items;
+		var history = await Clipboard.GetHistoryItemsAsync();
+		var t = history.Items;
 
-        if (t.Count > 0)
-            Clipboard.DeleteItemFromHistory(t[0]);
-        if (t.Count > 1)
-            Clipboard.SetHistoryItemAsContent(t[1]);
+		if (t.Count > 0)
+			Clipboard.DeleteItemFromHistory(t[0]);
+		if (t.Count > 1)
+			Clipboard.SetHistoryItemAsContent(t[1]);
 
-        return true;
-    }
+		return true;
+	}
 }
